89a795e3e0b99a8f5e8c467e526667d628bddacd
Ustiuzhanin Stanislav <ov.s.ust@gmail.com>
2018-01-25 22:08:40 +0300

    add env variable for deploy

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ set -e
 zip -r build.zip build
 
 curl -H "Content-type: application/zip" \
-     -H "Authorization: Bearer $CRYPTO" \
+     -H "Authorization: Bearer $TODO_APP" \
      --data-binary "@build.zip" \
      https://api.netlify.com/api/v1/sites
